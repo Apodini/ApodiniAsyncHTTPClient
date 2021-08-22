1389b99ed0d5534688372cc6b7ac236d0aad6c01
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2021-08-22 12:41:10 +0200

    Update to Apodini 0.4.0 and add REUSE compliance

--- Package.swift
+++ Package.swift
@@ -1,4 +1,12 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.5
+
+//
+// This source file is part of the Apodini AsyncHTTPClient open source project
+//
+// SPDX-FileCopyrightText: 2021 Paul Schmiedmayer and the project authors (see CONTRIBUTORS.md) <paul.schmiedmayer@tum.de>
+//
+// SPDX-License-Identifier: MIT
+//
 
 import PackageDescription
 
@@ -6,13 +14,13 @@ import PackageDescription
 let package = Package(
     name: "ApodiniAsyncHTTPClient",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v12)
     ],
     products: [
         .library(name: "ApodiniAsyncHTTPClient", targets: ["ApodiniAsyncHTTPClient"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Apodini/Apodini.git", .upToNextMinor(from: "0.3.0")),
+        .package(url: "https://github.com/Apodini/Apodini.git", .upToNextMinor(from: "0.4.0")),
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.5.0")
     ],
     targets: [
