efe030367d2e0605621d8a275ec4ef80318ffb9f
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2022-04-13 08:41:53 +0200

    Update to Apodini 0.9.X

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
         .library(name: "ApodiniAsyncHTTPClient", targets: ["ApodiniAsyncHTTPClient"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Apodini/Apodini.git", .upToNextMinor(from: "0.7.0")),
-        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.8.1")
+        .package(url: "https://github.com/Apodini/Apodini.git", .upToNextMinor(from: "0.9.0")),
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.9.0")
     ],
     targets: [
         .target(
